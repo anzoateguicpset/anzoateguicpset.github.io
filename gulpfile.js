47d8e5fb564575122aeefba08caf3733b9305d0e
franklin lugo <franklinlugo2805@gmail.com>
2017-03-28 12:10:53 -0400

    [add] sass support

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	rename = require("gulp-rename"),
+	browserSync = require('browser-sync').create();
 
 var config= {
 	browserSync: {
-		watch: [
-			'index.html',
-			'css/stylesheet.css'
-		]
-	}
+		watch: [ 'index.html', 'sassPath', 'cssPath']
+	},
+	sassPath: 'sass/*.scss',
+	cssPath: 'css'
 }
+
+gulp.task('sass', function(){
+	return gulp.src(config.sassPath)
+	.pipe(sass())
+    .pipe(rename({
+    	basename: "stylesheet",
+    	extname: ".css"
+    }))
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+});
 
 gulp.task('server', function(){
 	browserSync.init({
@@ -16,5 +28,8 @@ gulp.task('server', function(){
 	});
 	gulp.watch(config.browserSync.watch).on('change', browserSync.reload);
 });
+
+
+gulp.watch(config.sassPath, ['sass']);
 
 gulp.task('default', ['server']);
