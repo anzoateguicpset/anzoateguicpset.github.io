426daca64e2e71b74ba9a4b161faeb6ed43d7509
franklin lugo <franklinlugo2805@gmail.com>
2017-03-31 11:58:10 -0400

    [add] slide Show proyectos (has bug)

---
+++ js/js.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function(){
+	console.log("DOM Cargado");
+
+	// slideShow
+
+	var container = document.querySelector('.proyectos');
+	var element = document.querySelector('.proyectos section');
+	var prev = document.querySelector(".prev");
+	var next = document.querySelector(".next");
+
+	var slideIndex = 1;
+	showSlides(slideIndex);
+
+	prev.addEventListener("click", function(){
+		plusSlides(-1);	
+	});
+	next.addEventListener("click", function(){
+		plusSlides(1);
+	});
+
+	// funciones slideShow
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
+
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
+
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("slide");
+
+	  if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none";
+	  }
+	  
+	  slides[slideIndex-1].style.display = "block";
+	}
+
+
+});
