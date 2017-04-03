297c2b838e61964d2f9431c77187785eaa2f6c28
franklin lugo <franklinlugo2805@gmail.com>
2017-04-03 12:28:11 -0400

    [fix] bug initMap Function

--- js/js.js
+++ js/js.js
@@ -40,22 +40,23 @@ document.addEventListener('DOMContentLoaded', function(){
 	  slides[slideIndex-1].style.display = "block";
 	}
 
-	// API Google Maps
-
-	var footer = document.getElementById('footer');
-	var cpset = {lat: 10.148994, lng: -64.696688};
-
-	function initMap() {
-			
-		// map cpset
-	    var mapCpset = new google.maps.Map(footer, {
-	      zoom: 12,
-	      center: cpset
-	    });
-	    var marker = new google.maps.Marker({
-	      position: cpset,
-	      map: mapCpset
-	    });
-	}
 
 });
+
+// API Google Maps
+
+var footer = document.getElementById('footer');
+var cpset = {lat: 10.148994, lng: -64.696688};
+
+function initMap() {
+		
+	// map cpset
+    var mapCpset = new google.maps.Map(footer, {
+      zoom: 16,
+      center: cpset
+    });
+    var marker = new google.maps.Marker({
+      position: cpset,
+      map: mapCpset
+    });
+}
